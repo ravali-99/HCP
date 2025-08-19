62d5359df21ca2c5f75ded022f1a3b2053a3c5de
ravali-99 <ravali9498@gmail.com>
2025-08-19 05:36:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-00ca32bbc84273381"
+count = 2
+ami = "ami-0a232144cf20a27a5"
 instance_type = "t2.micro"
 tags = {
 Name = "hcp-server"
