48346008d0a55f2cf63a163a0e545abfdc42da5c
ravali-99 <ravali9498@gmail.com>
2025-08-19 05:42:42 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 2
-ami = "ami-0a232144cf20a27a5"
-instance_type = "t2.micro"
-tags = {
-Name = "hcp-server"
-}
-}
+  ami  = "ami-0de716d6197524dd9"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "raham-server"
+  }
